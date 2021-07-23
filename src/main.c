fc26676930ec44b67220364d1eddb966d0fb2db4
Billie-Cocaine <andrescorralesencontrastes@gmail.com>
2021-07-22 19:27:56 -0600

    new stuff

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
-// Aarón Sibaja || Garita aaron.sibajagarita@ucr.ac.cr
-// Andrés Corrales Castro || andres.corralescastro@ucr.ac.cr
-
+  //Andrés Corrales Castro || andres.corralescastro@ucr.ac.cr
+ // Aarón Sibaja Garita || aaron.sibajagarita@ucr.ac.cr
 			//---Programación Bajo Plataformaas Abiertas---//
 
 
+/**
+ * 
+ * @file main.c 
+ * @version 1.0
+ * @date July/2021
+ * @author Andrés Corrales Castro || andres.corralescastro@ucr.ac.cr
+ * @author Aarón Sibaja Garita || aaron.sibajagarita@ucr.ac.cr
+ * @brief Calculator for basic operations with integers,
+ * the application is implemented in a graphical interface created with GTK+3.
+ * 
+ * For the construction of the grphic interfce we used Glade aplication(version 3.22.1), Glade makes it easy
+ * to design the interface and assign properties to graphic objects.
+ * 
+ * @brief This application was built as final project for  
+ * the course of "Programación Bajo Plataformaas Abiertas", Escuela de Ingeniería Eléctrica,
+ * Universidad de Costa Rica. **/
 
 
 #include <stdlib.h>
@@ -12,21 +27,49 @@
 #include <stdio.h>
 
 
-/**VALUES CONTAINERS  
+/**@brief VALUES CONTAINERS 
  * 
- * value1  contains the first number to operate
- * value2  contains the second number to operate
- * value3  contains the result of the operation
+ *  
+ *@brief value1 and value2 are ser as char array for an easy way to concatenate 
+ * numbers in order to use strcat().
  * 
- * method is set to indicate wich is the arithmetic operation
- * 0-->addition
- * 1-->rest
- * 2-->multiplication
- * 3-->division 
  * 
- * door is set to indicate wich value to fill
- * 0-->fill value1
- * 1-->fill value2*/
+ * 
+ *  
+ * value1  contains the first number to operate.
+ * 
+ * value2  contains the second number to operate.
+ * 
+ * value3  contains the result of the operation.
+ * 
+ * method is set to indicate wich is the arithmetic operation.
+ * 
+ * 0-->addition.
+ * 
+ * 1-->rest.
+ * 
+ * 2-->multiplication.
+ * 
+ * 3-->division .
+ * 
+ * 
+ * 
+ * gate is set to indicate wich value to fill.
+ * 
+ * 0-->fill value1.
+ * 
+ * 1-->fill value2.
+ * 
+ * @code
+ 	char value1[100]="";
+	char value2[100]="";
+	float value3=0;
+	int method=0;
+	int gate=0;
+ * @endcode
+ * */
+
+
 
 
 
@@ -34,21 +77,59 @@ char value1[100]="";
 char value2[100]="";
 float value3=0;
 int method=0;
-int door=0;
+int gate=0;
 
 
-/* GTK OBJECTS  AND FUNCTIONS*/
 
 
 GtkWidget *lbl_SCREEN;
 
+/**
+ *GTK OBJECTS  AND FUNCTIONS
+ * @brief Here is where all graphicla objects are build and import from Glade template.
+ * 
+ * @code
+ * GtkWidget *lbl_SCREEN;
+ * int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+
+
+
+    gtk_init(&argc, &argv);
+
+
+    builder = gtk_builder_new(); // here is set a plece in memory for the  glade template
+    gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL); //this fucntion load a glade template fom a file
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main")); // this is set a place in memory for the main window graphical object
+    gtk_builder_connect_signals(builder, NULL);
+
+
+    lbl_SCREEN = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_SCREEN")); // here is set a label which is used as screen
+
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);                
+    gtk_main();
+
+
+    return 0;
+}
+ * @endcode
+ **/
 
 int main(int argc, char *argv[])
 {
     GtkBuilder      *builder; 
     GtkWidget       *window;
 
+
+
     gtk_init(&argc, &argv);
+
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
@@ -69,6 +150,12 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/**
+ * 
+ * @brief This function send a signal that closes the aplication
+ * 
+ ***/
+
 void on_window_main_destroy()
 {
     gtk_main_quit();
@@ -76,64 +163,116 @@ void on_window_main_destroy()
 
 
 
-/*  SET FUNTIONS TO THE ARITHIMETIC BOTTONS    */
+/**    
+ * 
+ * @brief Change the values for method and gate
+ * 
+ *  	gate=1;
+    	method=3;
 
-	// change the values for method and door
+ * */
+
+	
 
 	void on_operationDIV()
 	{
  
-		door=1;
+		gate=1;
     	method=3;
 
 	}
-
+/**    
+ * 
+ * @brief Change the values for method and gate
+ * 
+ * 		gate=1;
+    		method=2;
+ * */
 
 	void on_operationMULT()
 	{
 
-		door=1;
+		gate=1;
     	method=2;
 
 	}
+/**    
+ * 
+ * @brief Change the values for method and gate
+ * 
+ *   		gate=1;
+    		method=1;
 
+ * */
 
 	void on_operationREST(GtkWidget *widget, gpointer user_data)
 	{
 
-		door=1;
+		gate=1;
     	method=1;
 
 	}
 
 
+/**    
+ * 
+ * @brief Change the values for method and gate
+ * 
+ * 		gate=1;
+    		method=0;
+
+ * */
+
 	void on_operationSUM(GtkWidget *widget, gpointer user_data)
 	{
 
-		door=1;
+		gate=1;
     	method=0;
 		
 
 	}
-
+		/**
+		 * @brief This botom operate the values that the user set. This opertion is guide by the value of "method".
+		 * 
+		 * 
+		 * For the addition, rest ans multiplicatión firts is needed to convert values from value1 and value2
+		 * to integer, this values were set as string. In this way "atoi()" function transform string values
+		 * to integer,("a" and "b") are containers for the conversion of strings values.
+		 * @code
+				int a =atoi(value1); 
+    			int b =atoi(value2); 
+		 * 
+		 * @endcode
+		 * 
+		 * For division operation, values need to be transform from string to float, in order to
+		 * get a correct resoult. In this case "strtof()" is the fuction that works for it.
+		 * @code
+		 * 	char *pEND;
+				int a =strtof(value1, &pEND); 
+    			int b =strtof(value2, &pEND); 
+		 * 
+		 * @endcode
+		 * 
+		 * 
+		 **/
 	void on_btmRESULT(GtkWidget *widget, gpointer user_data)
 	{
 
 	
     if(method == 1) 
     {
-    	char s1[100];		//a,b are containers for conversion of string to float
-    	int a =atoi(value1); // atoi(mystring)--> is a fuction that converts
-    	int b =atoi(value2); // string to an integer 
+    	char s1[100];		
+    	int a =atoi(value1); 
+    	int b =atoi(value2);  
     	int value3=a-b;
     	sprintf(s1,"%d",value3);
     	gtk_label_set_text(GTK_LABEL(lbl_SCREEN),s1);
     }
     else if (method == 0)
     {
-    	char s1[100];	//a,b are containers for conversion of string to float
-    	int a =atoi(value1); // atoi(mystring)--> is a fuction that converts
-    	int b =atoi(value2); // string to an integer 
+    	char s1[100];	
+    	int a =atoi(value1); 
+    	int b =atoi(value2); 
     	int value3=a+b;
     	sprintf(s1,"%d",value3);
     	gtk_label_set_text(GTK_LABEL(lbl_SCREEN),s1);
@@ -141,9 +280,9 @@ void on_window_main_destroy()
     }
      else if (method == 2)
     {
-    	char s1[100];		//a,b are containers for conversion of string to float
-    	int a =atoi(value1); // atoi(mystring)--> is a fuction that converts
-    	int b =atoi(value2); // string to an integer 
+    	char s1[100];		
+    	int a =atoi(value1); 
+    	int b =atoi(value2);  
     	int value3=a*b;
     	sprintf(s1,"%d",value3);
     	gtk_label_set_text(GTK_LABEL(lbl_SCREEN),s1);
@@ -152,10 +291,6 @@ void on_window_main_destroy()
 
     else if (method == 3)
     {
-    	/*** a, b are containers for conversion of string to float
-    	 strtof(mystring, *ptr)--> is a function that converts
-    	 string to an float***/
-
     	char s1[100];
     	char *pEND;
     	float a, b; 				
@@ -168,7 +303,7 @@ void on_window_main_destroy()
     	}
     	else
     	{
-
+    		
     	float value3=a/b;
     		sprintf(s1,"%f",value3);
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN),s1);
@@ -185,7 +320,7 @@ void on_window_main_destroy()
 
     strcpy(value1,"");
     strcpy(value2,"");
-    door=0;
+    gate=0;
 
 
 
@@ -194,12 +329,24 @@ void on_window_main_destroy()
 
 	void on_btmERRASE(GtkWidget *widget, gpointer user_data)
 	{
-		/***
-		 * set all values to initial status 
+		/**
+		 * @brief Set all values to initial status .
 		 * 
 		 * 
 		 * strcpy(char1, char2)--> is a function that copy strings, 
-		 * copy char2 in char1
+		 * copy char2 in char1.
+		 * 
+		 * 
+		 * @code
+		 * 
+			strcpy(value1,"");
+   			strcpy(value2,"");
+			value3=0;
+			method=0;
+			gate=0;
+			gtk_label_set_text(GTK_LABEL(lbl_SCREEN), "");
+			@endcode
+
 		 **/
 
 
@@ -208,7 +355,7 @@ void on_window_main_destroy()
     strcpy(value2,"");
 	value3=0;
 	method=0;
-	door=0;
+	gate=0;
 	gtk_label_set_text(GTK_LABEL(lbl_SCREEN), "");
 
 
@@ -220,23 +367,30 @@ void on_window_main_destroy()
 
 
 
-/*  SET FUNTIONS TO THE NUMERIC BOTTONS    */
-	/**
-	*strcat(char1, char2)--> is a function that concatenate strings, add
-	*in the end of char1 the values of char2 
-	**/
+/* SET FUNTIONS TO THE NUMERIC BOTTONS */
 
-
-
-	void on_btm7(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 7 
+		/**
+		 * set number 7
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2 .
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
 
-		if(door)
+	void on_btm7(GtkWidget *widget, gpointer user_data)
+	{
+
+
+
+		if(gate)
     	{ 
     		strcat(value2, "7");
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -249,16 +403,26 @@ void on_window_main_destroy()
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value1);
     	}
 	}
-
-	void on_btm4(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 4 
+		/**
+		 * set number 4
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2.
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
 		 * 
 		*/
 
+	void on_btm4(GtkWidget *widget, gpointer user_data)
+	{
 
-		if(door)
+
+
+		if(gate)
     	{ 
 			strcat(value2,"4");
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -270,17 +434,28 @@ void on_window_main_destroy()
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value1);
     	}
 	}
-
-
-	void on_btm1(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 4 
+		/**
+		 * set number 1
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2.
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
 
-		if(door)
+	void on_btm1(GtkWidget *widget, gpointer user_data)
+	{
+
+
+
+		if(gate)
     	{ 
     		strcat(value2, "1"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -292,15 +467,26 @@ void on_window_main_destroy()
 
     	}    
 	}
-
-	void on_btm8(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 8 
+		/**
+		 * set number 8
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2. 
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
-		if(door)
+	void on_btm8(GtkWidget *widget, gpointer user_data)
+	{
+		
+
+		if(gate)
     	{ 
     		strcat(value2, "8"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -312,16 +498,27 @@ void on_window_main_destroy()
 
     	}	    
 	}
+		/**
+		 * set number 5
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2. 
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
+		 * 
+		*/
 
 	void on_btm5(GtkWidget *widget, gpointer user_data)
 	{
 
-		/***
-		 * set number 5 
-		 * 
-		*/
 
-		if(door)
+
+		if(gate)
     	{ 
     		strcat(value2, "5"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -334,15 +531,26 @@ void on_window_main_destroy()
     	}
 	}
 
-	void on_btm2(GtkWidget *widget, gpointer user_data)
-	{
-
-		/***
-		 * set number 2 
+		/**
+		 * set number 2
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2.
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
-	    if(door)
+	void on_btm2(GtkWidget *widget, gpointer user_data)
+	{
+
+
+	    if(gate)
     	{ 
     		strcat(value2, "2"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -354,16 +562,27 @@ void on_window_main_destroy()
 
     	}
 	}
-
-	void on_btm9(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 7 
+		/**
+		 * set number 9
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2. 
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
+	void on_btm9(GtkWidget *widget, gpointer user_data)
+	{
+	
 
-		if(door)
+
+		if(gate)
     	{ 
     		strcat(value2, "9"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -375,16 +594,27 @@ void on_window_main_destroy()
 
     	}
 	}
-
-	void on_btm6(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 7 
+		/**
+		 * set number 6
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2. 
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
 
-		if(door)
+	void on_btm6(GtkWidget *widget, gpointer user_data)
+	{
+
+
+		if(gate)
     	{ 
     		strcat(value2, "6"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -396,16 +626,27 @@ void on_window_main_destroy()
 
     	}
 	}
+		/**
+		 * set number 3
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2. 
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
+		 * 
+		*/
 
 	void on_btm3(GtkWidget *widget, gpointer user_data)
 	{
 
-		/***
-		 * set number 3 
-		 * 
-		*/
 
-		if(door)
+
+		if(gate)
     	{ 
     		strcat(value2, "3"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
@@ -417,16 +658,27 @@ void on_window_main_destroy()
 
     	}
 	}
-
-	void on_btm0(GtkWidget *widget, gpointer user_data)
-	{
-		/***
-		 * set number 0 
+			/**
+		 * set number 0
+		 * @brief  The values stored in the variable "gate" are evaluated,
+		 *  if "gate" is TRUE(gate=1), value2 is filled, for "gate" equal FALSE(gate=0), value1 is filled. 
+		 * The value of "gate" Changes when an operation button is clicked.
+		 * 
+		 * strcat(char1, char2)--> is a function that concatenate strings, add
+		 * in the end of char1 the values of char2. 
+		 * 
+		 * gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value#), this function shows on label screen
+		 * the values stored in variables "value1" and "value2".
+		 * 
 		 * 
 		*/
 
+	void on_btm0(GtkWidget *widget, gpointer user_data)
+	{
 
-		if(door)
+
+
+		if(gate)
     	{ 
     		strcat(value2, "0"); 
     		gtk_label_set_text(GTK_LABEL(lbl_SCREEN), value2);
